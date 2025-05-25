09dd037ebaa37218347a9ebedf356b59fcfbb749
Firox <86130692+FlR0X@users.noreply.github.com>
2025-05-25 22:54:52 +0200

    Add files via upload

---
+++ Internal Printsploit/Entry.cpp
@@ -0,0 +1,39 @@
+#include <Windows.h>
+
+#define GameModuleBase (reinterpret_cast<uintptr_t>(GetModuleHandleA("RobloxPlayerBeta.exe")))
+
+int ExecutePrints() {
+    uintptr_t printaddy = GameModuleBase + 0x14C8360; // roblox offset
+    using PrintFunc = int(__fastcall*)(int, const char*, ...);
+    auto PrintMessage = reinterpret_cast<PrintFunc>(printaddy);
+
+    enum class PrintLevel : int {
+        Normal = 0,
+        Info = 1,
+        Warning = 2,
+        Error = 3
+    };
+
+
+    while (true) {
+        PrintMessage(static_cast<int>(PrintLevel::Normal), "Normal // Updated");
+        Sleep(1000);
+        PrintMessage(static_cast<int>(PrintLevel::Info), "Info // Updated");
+        Sleep(1000);
+        PrintMessage(static_cast<int>(PrintLevel::Warning), "Warning // Updated");
+        Sleep(1000);
+        PrintMessage(static_cast<int>(PrintLevel::Error), "Error // Updated");
+        Sleep(1000);
+    }
+
+
+    return 1;
+}
+
+BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID) {
+    if (reason == DLL_PROCESS_ATTACH) {
+        ExecutePrints();
+        DisableThreadLibraryCalls(module);
+    }
+    return TRUE;
+}
